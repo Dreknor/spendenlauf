d750606434ea340775bae31ae3075d797ea1ddb0
Dreknor <pauli84@freenet.de>
2020-03-11 07:37:23 +0100

    Link Bildrechte

--- resources/views/laeufer/create.blade.php
+++ resources/views/laeufer/create.blade.php
@@ -84,7 +84,7 @@
                             </div>
 
                             <div class="col-md-10 col-sm-10">
-                                <label for="geschlecht" class="">{{ __('Ich bestätige, dass der Läufer (bzw. der Sorgeberechtigte des Läufers) mit den Teilnahmebedingungen einverstanden ist.') }}</label>
+                                <label for="geschlecht" class="">Ich bestätige, dass der Läufer (bzw. der Sorgeberechtigte des Läufers) mit den <a href="https://spendenlauf-radebeul.de/teilnahmebedingungen.pdf" target="_blank">Teilnahmebedingungen</a> und dem <a href="https://spendenlauf-radebeul.de/Datenschutz.pdf" target="_blank">Datenschutz</a> einverstanden ist.</label>
                             </div>
                         </div>
                         <div class="form-group row">
@@ -93,7 +93,7 @@
                             </div>
 
                             <div class="col-md-10 col-sm-10">
-                                <label for="bilder" class="">{{ __('Ich bestätige, dass der Läufer (bzw. der Sorgeberechtigte des Läufers) das Fotografieren und die Verwendung der Fotos gemäß Fotofreigabe genehmigt.') }}</label>
+                                <label for="bilder" class="">Ich bestätige, dass der Läufer (bzw. der Sorgeberechtigte des Läufers) das Fotografieren und die Verwendung der Fotos gemäß <a href="https://spendenlauf-radebeul.de/Bildrechte.pdf" target="_blank">Fotofreigabe</a> genehmigt.</label>
                             </div>
                         </div>
 
