ff5d65ddb05637bda2a04864dc1e31b491c0674b
Shift <shift@laravelshift.com>
2022-01-25 18:38:59 +0000

    Shift bindings
    PHP 5.5.9+ adds the new static `class` property which provides the fully qualified class name. This is preferred over using class name strings as these references are checked by the parser.

--- database/factories/SponsoringFactory.php
+++ database/factories/SponsoringFactory.php
@@ -8,7 +8,7 @@ $factory->define(\App\Model\Sponsoring::class, function (Faker $faker) {
     return [
         'sponsor_id'    => $faker->randomFloat(2, 1, 50),
         'verwaltet_von' => $faker->randomFloat(2, 1, 20),
-        'sponsorable_type'  => $faker->randomElement(['App\Model\Laeufer', 'App\Model\Teams']),
+        'sponsorable_type'  => $faker->randomElement([\App\Model\Laeufer::class, \App\Model\Teams::class]),
         'sponsorable_id' => $faker->randomFloat(2, 1, 20),
         'rundenBetrag'  => $faker->randomElement([0, 0, 0, $faker->randomFloat(1, 0, 25)]),
         'festBetrag'  => $faker->randomElement([0, $faker->randomFloat(1, 0, 500)]),
