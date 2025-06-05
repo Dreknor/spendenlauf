d495453b0dd532dbffb75f293220d71d4c8bd576
Dreknor <Dreknor@users.noreply.github.com>
2025-06-05 19:34:45 +0200

    Add BCC to sponsor email notifications in SponsorController

--- app/Http/Controllers/SponsorController.php
+++ app/Http/Controllers/SponsorController.php
@@ -188,7 +188,9 @@ class SponsorController extends Controller
                 })->count();
 
 
-                    Mail::to($sponsor->email)->send(new SponsorAnschreiben($sponsor, $repository->anzahlLauefer(), $repository->spendensumme(), $sponsoring_projects));
+                    Mail::to($sponsor->email)
+                        ->bcc(config('mail.from.address'))
+                        ->send(new SponsorAnschreiben($sponsor, $repository->anzahlLauefer(), $repository->spendensumme(), $sponsoring_projects));
                 $sponsor->update(['mail_send' => Carbon::now()]);
             }
         }
